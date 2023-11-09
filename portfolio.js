82de57e0d66bc051c8405afe43c1dd11cfc7aa19
srishti-tripathi <150232631+srishti-tripathi@users.noreply.github.com>
2023-11-09 08:47:41 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,48 @@
+let menuIcon= document.querySelector('#menu-menu');
+let navbar= document.querySelector('.navbar');
+
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+   navbar.classList.toggle('active');
+};
+
+
+
+
+let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll= () => {
+section.forEach(sec =>{
+    let top = window.scrollY;
+let offset = sec.offsetTop-150;
+let height = sec.offsetHeight;
+let id = sec.getAttribute ('id');
+
+if(top >= offset && top<offset + height){
+navlinks.forEach(links=>{
+    links.classList.remove('active');
+    document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+    
+});
+};
+});
+let header=document.querySelector('header');
+header.classList.toggle('sticky',windiw.scrollY > 100);
+
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+};
+
+
+ScrollReveal({ 
+reset: true,
+distance:'80px',
+duration:2000,
+delay:200
+});
+
+ScrollReveal().reveal('.home-content,.heading',{ origin: 'top' });
+ScrollReveal().reveal('.home-img,.services-container,.contact form', { origin: 'bottom'});
